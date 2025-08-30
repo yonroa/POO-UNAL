c80356e96321a5e215b30b4e11370989361a610d
Yon Anderson Roa <91107387+yonroa@users.noreply.github.com>
2025-08-30 13:44:02 -0500

    Create Ejercicio_5.py

---
+++ Actividad 1/Ejercicio_5.py
@@ -0,0 +1,20 @@
+class Calculos:
+
+  def calcular_suma(suma, x):
+    return (suma + x)
+
+  def calcular_x(x, y):
+    return (x + (y ** 2))
+
+  def calcular_suma_final(suma, x, y):
+    return (suma + (x / y))
+
+x = float(input('Escriba el valor de x: '))
+suma = float(input('Escriba el valor de SUMA: '))
+y = float(input('Escriba el valor de Y: '))
+
+suma = Calculos.calcular_suma(suma, x)
+x = Calculos.calcular_x(x, y)
+suma = Calculos.calcular_suma_final(suma, x, y)
+
+print("EL VALOR DE LA SUMA ES: ", suma)
